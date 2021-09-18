4537d2274b60cc6b72ac99bf5a62dd043dc2f214
ecovaci <eugen.covaci.q@gmail.com>
2021-09-18 12:44:28 +0300

    More refactoring: code simplification

--- src/main/java/org/kpax/winfoom/util/Throwables.java
+++ src/main/java/org/kpax/winfoom/util/Throwables.java
@@ -28,58 +28,6 @@ import java.util.Optional;
 public class Throwables {
 
     /**
-     * Returns the first {@link Throwable} object in the exception chain that matches one of the provided types.
-     *
-     * @param throwable the throwable to inspect (not null)
-     * @param types     the filtering types
-     * @return the {@link Optional} wrapping the root cause
-     */
-    public static Optional<Throwable> getRootCause(Throwable throwable, Class<?>... types) {
-        Assert.notNull(throwable, "throwable cannot be null");
-        if (types == null || types.length == 0) {
-            return Optional.of(ExceptionUtils.getRootCause(throwable));
-        }
-        List<Class<?>> typesList = Arrays.asList(types);
-        return ExceptionUtils.getThrowableList(throwable).stream().
-                filter(e -> typesList.stream().anyMatch(type -> type.isAssignableFrom(e.getClass()))).findAny();
-    }
-
-    /**
-     * Throw the {@link Throwable} only if it is unchecked, does nothing otherwise.
-     *
-     * @param throwable the {@link Throwable} instance
-     */
-    public static void throwIfUnchecked(Throwable throwable) {
-        if (throwable instanceof RuntimeException) {
-            throw (RuntimeException) throwable;
-        }
-        if (throwable instanceof Error) {
-            throw (Error) throwable;
-        }
-    }
-
-    /**
-     * Check if a {@link Throwable} instance is an unchecked exception
-     *
-     * @param throwable the {@link Throwable} instance to be inspected
-     * @return {@code true} iff it is an unchecked exception
-     */
-    public static boolean isUnchecked(Throwable throwable) {
-        return throwable instanceof RuntimeException || throwable instanceof Error;
-    }
-
-    /**
-     * Check if a {@link Throwable} instance is a checked exception
-     *
-     * @param throwable the {@link Throwable} instance to be inspected
-     * @return {@code true} iff it is a checked exception
-     * @see #isUnchecked(Throwable)
-     */
-    public static boolean isChecked(Throwable throwable) {
-        return !isUnchecked(throwable);
-    }
-
-    /**
      * If the {@link Exception} class is the same as {@code cls} or a subclass of it,
      * then cast the {@link Exception} instance to {@code cls} and throw it, otherwise do nothing.
      *
